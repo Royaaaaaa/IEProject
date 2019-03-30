1008d75f60c58e33cc5aa4f2c93d6f8fd849b91a
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2019-03-30 18:41:58 +1100

    index page

--- APP01/views.py
+++ APP01/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import *
+
+from .models import *
+
+def index(request):
+    return render(request,'app01/index.html')
